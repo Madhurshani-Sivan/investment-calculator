6024bb615373fcfe910e6b4b986575a6f025717e
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-07-21 15:34:29 +0530

    handle submit and reset button click events

--- src/components/UserInput/Input.js
+++ src/components/UserInput/Input.js
@@ -1,8 +1,18 @@
 const Input = (props) => {
+  const inputChangeHandler = (input, value) => {
+    console.log(input, value);
+  };
+
   return (
     <p>
       <label htmlFor={props.input.id}>{props.input.value}</label>
-      <input type="number" id={props.input.id} />
+      <input
+        type="number"
+        id={props.input.id}
+        onChange={(event) =>
+          inputChangeHandler(props.input.id, event.target.value)
+        }
+      />
     </p>
   );
 };
--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -8,12 +8,22 @@ const UserInput = () => {
     { id: "expected-return", value: "Expected Interest (%, per year)" },
     { id: "duration", value: "Investment Duration (years)" },
   ];
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("submit");
+  };
+
+  const resetHanlder = () => {
+    console.log("reset");
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
       <InputGroup inputs={inputs.slice(0, 2)} />
       <InputGroup inputs={inputs.slice(2)} />
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" className="buttonAlt" onClick={resetHanlder}>
           Reset
         </button>
         <button type="submit" className="button">
